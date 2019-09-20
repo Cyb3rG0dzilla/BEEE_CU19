0c8939ae3df466c57b48c416e8af8122be7f382b
Ankur Acharjee <29690304+Cyb3rG0dzilla@users.noreply.github.com>
2019-09-20 18:43:56 +0530

    Add files via upload

---
+++ Exp1_LED_Flasher/Blink.ino
@@ -0,0 +1,11 @@
+void setup() {
+
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(10, HIGH);
+  delay(1000);                    
+  digitalWrite(10, LOW);    
+  delay(1000);                      
+}
