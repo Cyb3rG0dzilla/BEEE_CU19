63917ca5ea02973c580e55f51e606d9c98732e74
Ankur Acharjee <29690304+Cyb3rG0dzilla@users.noreply.github.com>
2019-09-20 20:22:28 +0530

    Add files via upload

---
+++ EXP2_LED_Chaser/LED_chaser_1.ino
@@ -0,0 +1,29 @@
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  delay(500);
+  digitalWrite(2, HIGH);
+  digitalWrite(1,LOW);
+  delay(500);
+  digitalWrite(3,HIGH);
+  digitalWrite(2,LOW);
+  delay(500);
+  digitalWrite(4,HIGH);
+  digitalWrite(3,LOW);
+  delay(500);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  delay(500);
+}
