077d18bcf8340db9735c55f3b5868a44ef79a27e
Ankur Acharjee <29690304+Cyb3rG0dzilla@users.noreply.github.com>
2019-11-03 23:53:59 +0530

    Add files via upload

---
+++ Exp7_CLCD/Digital_Data_Display_System.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+
+  lcd.begin(16, 2);
+
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print("hello world");
+}
